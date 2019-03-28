22a10b72c3a7dd2bde215a17dc9270491b184de0
Jens Andresen <jandr@DFDS.COM>
2019-03-28 15:55:25 +0100

    add debug statements

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ const port = process.env.PORT || 9321;
 const target = process.env.PROXY_TO || "http://localhost:9322";
 const expectedAdGroup = process.env.AD_GROUP || "";
 const isPassThrough = !expectedAdGroup || expectedAdGroup == "";
+const isDebug = (process.env.DEBUG || "0") == "1";
 
 if (isPassThrough) {
     console.log("AD_GROUP environment variable NOT set - running as passthrough proxy.");
@@ -32,6 +33,11 @@ function isAuthorized(req) {
     if (bearerToken) {
         const payload = jwt.decode(bearerToken);
         const groups = new Set(payload.groups || []);
+
+        if (isDebug) {
+            console.log("JWT: " + JSON.stringify(payload));
+        }
+
         return groups.has(expectedAdGroup);
     }
 
@@ -40,6 +46,10 @@ function isAuthorized(req) {
 
 const proxy = httpProxy.createProxyServer({ target: target });
 const proxyServer = http.createServer((req, res) => {
+    if (isDebug) {
+        console.log("Incomming headers: " + JSON.stringify(req.headers));
+    }
+
     res.setHeader("x-adoorman-mode", isPassThrough ? "passthrough" : "authorize");
 
     if (isAuthorized(req)) {
