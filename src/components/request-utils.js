01762f3e1045ea1ec399623799f0ed0ed6894ccf
Jens Andresen <jensandresen@gmail.com>
2019-04-08 09:28:37 +0200

    wip: request utils

--- src/components/request-utils.js
+++ src/components/request-utils.js
@@ -9,3 +9,49 @@
 // exports.getCookieExpirationTime = function (req) {
 
 // }
+
+const identity = "x-amzn-oidc-identity";
+const accessToken = "x-amzn-oidc-identity";
+
+const headers = {
+    identity,
+    accessToken,
+    jwt: "x-amzn-oidc-data"
+};
+
+export { headers as headerNames };
+
+export default class RequestUtils {
+    constructor(options) {
+        this.jwtUtils = options.jwtUtils;
+
+        this.getHeaderValue = this.getHeaderValue.bind(this);
+        this.getIdentityFrom = this.getIdentityFrom.bind(this);
+        this.getAccessTokenFrom = this.getAccessTokenFrom.bind(this);
+        this.extractExpirationDateFrom = this.extractExpirationDateFrom.bind(this);
+    }
+
+    getHeaderValue(req, name) {
+        const headers = req.headers || {};
+        return headers[name] || null;
+    }
+
+    getIdentityFrom(req) {
+        return this.getHeaderValue(req, headers.identity);
+    }
+
+    getAccessTokenFrom(req) {
+        return this.getHeaderValue(req, headers.accessToken);
+    }
+
+    extractExpirationDateFrom(req) {
+        const headerValue = this.getHeaderValue(req, headers.jwt);
+
+        if (!headerValue) {
+            return null;
+        }
+        
+        const jwt = this.jwtUtils.deserialize(headerValue);
+        return new Date(jwt.exp*1000);
+    }
+}
---
+++ src/tests/test-requestutils.js
@@ -0,0 +1,113 @@
+import assert from "assert";
+import RequestUtils, { headerNames } from "./../components/request-utils";
+
+const reqBuilder = (options) => {
+    const defaults = {
+        headers: {},
+        setHeader: function(name, value) {
+            this.headers[name] = value;
+        }
+    };
+    return {...defaults, ...options};
+};
+
+const buildSut = (options) => {
+    const defaults = {
+        jwtUtils: {
+            deserialize: () => null
+        }
+    };
+
+    options = {...defaults, ...options};
+    options.jwtUtils = {...defaults.jwtUtils, ...options.jwtUtils};
+
+    return new RequestUtils(options);
+};
+
+describe("RequestUtils", () => {
+    describe("getIdentityFrom", () => {
+
+        it("returns expected when identity is NOT available in request", () => {
+            const expected = null;
+
+            const sut = buildSut();
+
+            const req = reqBuilder();
+            const result = sut.getIdentityFrom(req);
+
+            assert.equal(result, expected, `Expected "${expected}" but got "${result}"`);
+        });
+
+        it("returns expected identity from request when available", () => {
+            const expected = "foo";
+
+            const req = reqBuilder();
+            req.setHeader(headerNames.identity, expected);
+
+            const sut = buildSut();
+            const result = sut.getIdentityFrom(req);
+
+            assert.equal(result, expected, `Expected "${expected}" but got "${result}"`);
+        });
+
+    });
+
+    describe("getAccessTokenFrom", () => {
+
+        it("returns expected when access token is NOT available in request", () => {
+            const expected = null;
+
+            const sut = buildSut();
+
+            const req = reqBuilder();
+            const result = sut.getAccessTokenFrom(req);
+
+            assert.equal(result, expected, `Expected "${expected}" but got "${result}"`);
+        });
+
+        it("returns expected access token from request when available", () => {
+            const expected = "foo";
+
+            const req = reqBuilder();
+            req.setHeader(headerNames.accessToken, expected);
+
+            const sut = buildSut();
+            const result = sut.getAccessTokenFrom(req);
+
+            assert.equal(result, expected, `Expected "${expected}" but got "${result}"`);
+        });
+
+    });
+
+    describe("extractExpirationDateFrom", () => {
+
+        it("returns expected when NOT available", () => {
+            const expected = null;
+
+            const sut = buildSut();
+
+            const reqStub = reqBuilder();
+            const result = sut.extractExpirationDateFrom(reqStub);
+
+            assert.equal(result, expected, `Expected "${expected}" but got "${result}"`);
+        });
+
+        it("returns expected when available", () => {
+            const expected = new Date();
+
+            const sut = buildSut({
+                jwtUtils: { deserialize: () => {
+                    return { exp: Math.floor(expected.getTime()/1000) };
+                }}
+            });
+
+            const reqStub = reqBuilder();
+            reqStub.setHeader(headerNames.jwt, "dummy-jwt");
+
+            const result = sut.extractExpirationDateFrom(reqStub);
+
+            assert.equal(result, expected, `Expected "${expected}" but got "${result}"`);
+        });
+
+    });
+});
